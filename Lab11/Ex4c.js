ec8e42c825cd9b21e260fa51dab2b9457e6460dd
nagaimeg <nagaimeg@hawaii.edu>
2020-10-22 02:38:58 -1000

    Invoice4

--- Lab11/Ex4c.js
+++ Lab11/Ex4c.js
@@ -1,7 +1,7 @@
 function isNonNegIntString(string_to_check, returnErrors=false) {
     /*this function returns ture if string_to_check is a non-negative integer.If returnErrors=true it will return the array of reaons it is non-negative integer
-    errors = []; // assume no errors at first
     */
+   errors = []; // assume no errors at first
     if (Number(string_to_check) != string_to_check) errors.push('Not a number!'); // Check if string is a number value
     if (string_to_check < 0) errors.push('Negative value!'); // Check if it is non-negative
     if (parseInt(string_to_check) != string_to_check) errors.push('Not an integer!'); // Check that it is an integer
@@ -16,4 +16,4 @@ for(i in pieces) {
     console.log(`${pieces[i]} is non neg int ${isNonNegIntString(pieces[i], true).join("***")}`);
 
 }
-console.log(isNonNegIntString('5.00'));
+console.log(isNonNegIntString(-1));
