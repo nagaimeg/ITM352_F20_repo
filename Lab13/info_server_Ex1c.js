eedd11f73150475b742293e056a4a706dc1530ef
nagaimeg <nagaimeg@hawaii.edu>
2020-11-05 13:56:38 -1000

    Lab13

---
+++ Lab13/info_server_Ex1c.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path);
+    next();
+});
+
+app.get('/hello.txt', function (request, response, next) {
+    response.send('Got a GET to /test request');
+    next();
+});
+
+app.use(express.static('./public'));
+
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here
---
+++ Lab13/public/product_data.js
@@ -0,0 +1,15 @@
+products = 
+[
+  {  
+  "model":"Apple iPhone XS",  
+  "price": 1000.00
+  },
+  {  
+  "model":"Samsung Galaxy",  
+  "price": 240.00  
+  }
+];
+
+if(typeof module != 'undefined') {
+  module.exports.products = products;
+}
