ddc84f4eb547a44806a055f420768b49f2f75845
nagaimeg <nagaimeg@hawaii.edu>
2020-11-06 21:15:08 -1000

    update assignment1

--- Nagai_Meghan_Assignment1/server.js
+++ Nagai_Meghan_Assignment1/server.js
@@ -1,10 +1,11 @@
-//copied from Lab13 Ex4  and heavily adapted to display the invoice. Looked at the Assignemnt 1 Example for inspiration
+//copied from Lab13 Ex4  and heavily adapted to display the invoice.
 var data = require('./products.json');//loads the product.json
 var products = data.products;//sets the products_array variables
 var express = require('express');//enabling the usage of the express module
 var app = express();//setting the express module as app
 var myParser = require("body-parser");//loading and enabling the body parser module
 var fs = require('fs');// references fs module to read the file path to invoice template
+//const { response } = require('express');
 
 
 //writes the request message
@@ -14,44 +15,43 @@ app.all('*', function (request, response, next) {
 });
 
 app.use(myParser.urlencoded({ extended: true }));
-//telling to run the display_invoice function when data is posted assuming not undefines
-app.post("/process_form", function (request, response, next) {
-    let POST = request.body;
-    if(typeof POST['submit_button'] != 'undefined') {
+//telling to run the display_invoice function when data is posted assuming not undefined (copied from Lab 13)
+app.post("/process_form", function (request, response) {
+    //POST = request.body;
         display_invoice(request.body, response);
-    } 
-    var contents = fs.readFileSync('./views/invoice.template', 'utf8');//uses the invoice template which was adapted from invoice4
-    response.send(eval('`' + contents + '`')); // render template string
+    }); 
+
 
     //computes the values needed to display the invoice
-    function display_invoice() {
+    function display_invoice(POST,response) {
+        if(typeof POST['submit_button'] != 'undefined') {
+            var contents = fs.readFileSync('./views/invoice.template', 'utf8');//uses the invoice template which was adapted from invoice4
+        invoice = '';
         subtotal = 0;
-        str = '';
-        for (i = 0; i < products.length; i++) {
-            qty = 0; //set starting quantity to 0
-            if(typeof POST[`quantity${i}`] != 'undefined') {
-                qty = POST[`quantity${i}`];
-            } //sets the quantity amount for each product
+        for (i in products) {
+        qty = POST[`quantity${i}`];
+             //sets the quantity amount for each product
             if (qty > 0) {
-                // displays the product row copites from WOD Invoice4 invoice.html
+                // displays the product rows of items ordered. Copied from WOD Invoice4 invoice.html
                 extended_price =qty * products[i].price
-                subtotal += extended_price;
-                str += (`
+                subtotal= subtotal + extended_price;
+                invoice += (`
                 <tr>
-                <td align="center">${products[i].brand} ${products[i].product_name}</td>
+                <td style="text-align: left;">${products[i].brand} ${products[i].product_name}</td>
                 <td align="center">${qty}</td>
-                <td align="center">\$${products[i].price}</td>
-                <td align="center">\$${extended_price}</td>
+                <td style="text-align: center"\$${products[i].price}</td>
+                <td style="text-align: right;">\$${extended_price.toFixed(2)}</td>
               </tr>
       `);
             }
         }
-        // Compute tax
+
+        // Tax
         //From WOD Invoice4 Invoice.html 
         tax_rate = 0.0575;
         sales_tax = tax_rate * subtotal;
 
-        // Compute shipping
+        // Shipping Costs
         //From WOD Invoice4  Invoice.html
         if (subtotal <= 50) {
             shipping_cost = 2;
@@ -63,16 +63,19 @@ app.post("/process_form", function (request, response, next) {
             shipping_cost = 0.05 * subtotal; // 5% of subtotal
         }
 
-        // Compute grand total
+        // Total Amount
         //From WOD Invoice4  Invoice.html
         total = subtotal + sales_tax + shipping_cost;
+
+        if (isNonNegInt(qty)) {
+            response.send(eval('`' + contents + '`'));
+            } else {
+                response.send(`<h3>You have entered an invalid quantity ${qty}! Please go back and check your orders</font></h3>`);
+            }
         
-        return str;
     }
-
-});
-
-
+    }
+//copied from Lab13 Ex4
 function isNonNegInt(q, returnErrors = false) {
     errors = []; // assume no errors at first
     if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
