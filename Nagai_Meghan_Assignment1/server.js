ba17f16551765ce3b8f691afe8fc176b79354142
nagaimeg <nagaimeg@hawaii.edu>
2020-11-07 00:15:51 -1000

    update assignment1

--- Nagai_Meghan_Assignment1/server.js
+++ Nagai_Meghan_Assignment1/server.js
@@ -1,6 +1,6 @@
 //copied from Lab13 Ex4  and heavily adapted to display the invoice.
 var data = require('./products.json');//loads the product.json
-var products = data.products;//sets the products_array variables
+var products_array = data.products_array;//sets the products_array variables
 var express = require('express');//enabling the usage of the express module
 var app = express();//setting the express module as app
 var myParser = require("body-parser");//loading and enabling the body parser module
@@ -23,7 +23,7 @@ app.post("/process_form", function (request, response) {
     //assume no quantities
     hasquantities= false;
     //check if no errors if error true, check if has quantities if there are, then true
-    for (i in products) {
+    for (i in products_array) {
         qty = request.body[`quantity${i}`];
         if(qty>0) {
             hasquantities=true;
@@ -48,18 +48,18 @@ function display_invoice(POST, response) {
 
         invoice_rows = '';
         subtotal = 0;
-        for (i in products) {
+        for (i in products_array) {
             qty = POST[`quantity${i}`];
             //sets the quantity amount for each product
             if (qty > 0) {
                 // displays the product rows of items ordered. Copied from WOD Invoice4 invoice.html
-                extended_price = qty * products[i].price
+                extended_price = qty * products_array[i].price
                 subtotal = subtotal + extended_price;
                 invoice_rows += (`
                 <tr>
-                <td style="text-align: left;">${products[i].brand} ${products[i].product_name}</td>
+                <td style="text-align: left;">${products_array[i].brand} ${products_array[i].product_name}</td>
                 <td align="center">${qty}</td>
-                <td style="text-align: center">\$${products[i].price}</td>
+                <td style="text-align: center">\$${products_array[i].price}</td>
                 <td style="text-align: right;">\$${extended_price.toFixed(2)}</td>
               </tr>
       `);
