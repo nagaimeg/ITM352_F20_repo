b79cef6568938b7b09f7bed257bb9167b339ced2
nagaimeg <nagaimeg@hawaii.edu>
2020-12-09 21:18:15 -1000

    Lab15

--- Lab15/Ex2.js
+++ Lab15/Ex2.js
@@ -3,13 +3,17 @@ var app = express();
 var myParser = require("body-parser");
 const fs = require('fs');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 app.use(cookieParser());
+var store =
+    app.use(session({ secret: "ITM352 rocks!", store: store }));
+
 
 const user_data_filename = 'user_data.json';
 
 //check if file exists before reading it
 if (fs.existsSync(user_data_filename)) {
-
     stats = fs.statSync(user_data_filename);
     console.log(`user_data.json has ${stats['size']} characters`);
 
@@ -23,18 +27,33 @@ if (fs.existsSync(user_data_filename)) {
 
 app.use(myParser.urlencoded({ extended: true }));
 
+app.all("*", function (request, response, next) {
+    if (!request.session.lastLogin) {
+        request.session.lastLogin = null;
+    }
+    console.log(request.session, request.cookies);
+    next();
+});
+//This makes sure that the session package is working
+app.get("/use_session", function (request, response) {
+    console.log(`session id is`)
+    if (typeof request.session.id != 'undefined') {
+        response.send(`welcome your session ID is ${request.session.id}`)
+    }
+});
+
 app.get("/set_cookie", function (request, response) {
-    response.cookie('myname', 'Meghan', {maxAge: 5*1000});
-    response.send ('cookie sent!');
+    response.cookie('myname', 'Meghan', { maxAge: 5 * 1000 });
+    response.send('cookie sent!');
 });
 
 app.get("/use_cookie", function (request, response) {
     console.log(request.cookies);
     thename = 'ANONYMOUS';
-    if(typeof request.cookies['myname'] != 'undefined') {
+    if (typeof request.cookies['myname'] != 'undefined') {
         thename = request.cookies['myname'];
     }
-    response.send('Welcome to the Use Cookie page Meghan ');
+    response.send(`Welcome to the Use Cookie page ${thename}`);
 });
 
 app.get("/register", function (request, response) {
@@ -51,9 +70,9 @@ app.get("/register", function (request, response) {
 </body>
     `;
     response.send(str);
- });
+});
 
- app.post("/process_register", function (request, response) {
+app.post("/process_register", function (request, response) {
     // process a simple register form
     //validate the reg info
 
@@ -67,13 +86,20 @@ app.get("/register", function (request, response) {
     //write updated object to user_data_filename
     reg_info_str = JSON.stringify(users_reg_data);
     fs.writeFileSync(user_data_filename, reg_info_str);
- });
+});
 
 
 app.get("/login", function (request, response) {
     // Give a simple login form
+
+    if (request.session["lastLogin"]) {
+        lastLogin = request.session["lastLogin"]
+    } else {
+        lastLogin = 'First login!';
+    }
     str = `
 <body>
+Last Login: ${lastLogin}
 <form action="process_login" method="POST">
 <input type="text" name="username" size="40" placeholder="enter username" ><br />
 <input type="password" name="password" size="40" placeholder="enter password"><br />
@@ -91,12 +117,21 @@ app.post("/process_login", function (request, response) {
     //if username exists, get password
     if (typeof users_reg_data[request.body.username] != 'undefined') {
         if (request.body.password == users_reg_data[request.body.username].password) {
-            response.send(`Thank you ${request.body.username} for logging in!`);
+            if (request.body.password == users_reg_data[request.body.username].password) {
+
+                var now = new Date();
+
+                request.session.lastLogin = now.getTime();
+
+                console.log(`${request.body.username} logged in on ${request.session.lastLogin}`);
+
+                response.send(`Thank you ${request.body.username} for logging in.`);
+            }
         } else {
             response.send(`Hey! ${request.body.password} does not match the password we have for you!`);
 
         }
-    } else { response.send(`Hey! ${request.body.username} does not exist!`) }
+    } else { response.send(`Hey! ${request.body.username} does not exist!`); }
 
 });
 
